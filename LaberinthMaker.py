37b21b43c6e3b1a45fd7f51c0604182c924f167c
TheJPMZ <64183934+TheJPMZ@users.noreply.github.com>
2021-06-12 05:48:46 -0500

    Alpha push

---
+++ LaberinthMaker.py
@@ -0,0 +1,35 @@
+import random
+
+
+pos = [0, 0]
+walls = []
+path = []
+Start = ()
+Here = ()
+
+# Selecciona un lugar donde empezar
+def PressStart():
+  pos[0] = random.randint(0, 24)
+  pos[1] = random.randint(0, 14)
+  walls.append(pos)
+  
+# Moves to a clean path
+#
+def Move():
+    print("comequeques")
+
+  
+
+
+
+
+   
+
+
+
+
+
+PressStart()
+
+
+print(pos)
